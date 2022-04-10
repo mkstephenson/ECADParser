60ea06be1ab885cdc43fe7ca83469fb18b9bec98
Michael Stephenson <m.k.stephenson@outlook.com>
2022-04-10 15:19:04 +0200

    Added RabbitMQFiller

---
+++ RabbitMQQueueFiller/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Text.Unicode;
+
+var exchangeName = "x.eca";
+var queueName = "q.eca";
+var routingKey = "eca";
+
+var connectionFactory = new ConnectionFactory();
+
+connectionFactory.HostName = "nas.n12.eu";
+
+var conn = connectionFactory.CreateConnection();
+
+var channel = conn.CreateModel();
+
+channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout);
+channel.QueueDeclare(queueName, true, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+foreach (var file in Directory.EnumerateFiles("\\\\nas\\web\\eca-json"))
+{
+  var name = Path.GetFileName(file);
+  var content = new ReadOnlyMemory<byte>(Encoding.UTF8.GetBytes(Path.Combine("http://nas.n12.eu/eca-json/", name)));
+  channel.BasicPublish(exchangeName, routingKey, null, content);
+}
